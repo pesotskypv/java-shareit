5cc28d63538142690a9a0c429cad19c257d9cbc6
Pavel Pesotsky <pesotskypv@yandex.ru>
2023-12-14 21:01:55 +0300

    fix: метод findItemsByOwnerIdOrderByOwnerIdAsc(Long id)

--- server/src/main/java/ru/practicum/shareit/item/dao/ItemRepository.java
+++ server/src/main/java/ru/practicum/shareit/item/dao/ItemRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    List<Item> findItemsByOwnerId(Long id);
+    List<Item> findItemsByOwnerIdOrderByOwnerIdAsc(Long id);
 
     @Query("select i from Item i " +
             "where i.available = true and (upper(i.name) like upper(concat('%', ?1, '%')) " +
